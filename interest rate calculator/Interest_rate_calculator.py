f2361b462c7464337b48b6651a3092c0e5e8b6e8
Adam Power Gonzalez <72943449+mrpowergonz@users.noreply.github.com>
2023-07-29 14:55:24 +0200

    Add files via upload

---
+++ interest rate calculator/Interest_rate_calculator.py
@@ -0,0 +1,20 @@
+#collect the necesary inputs : principal , apr , years
+#calculate the monthly payment
+#show to the user
+
+def main():
+    print(" This is monthly payment loan calculator ")
+    print(" ")
+
+
+    principal = float(input("Input the loan amount: "))
+    apr = float(input("Input the anual interest rate: "))
+    years = int(input("Input the amount of years: "))
+
+    monthly_interest_rate = apr / 1200
+    amount_of_months = years * 12
+    monthly_payment = principal * monthly_interest_rate /(1 - (1 + monthly_interest_rate) ** (- amount_of_months))
+
+    print("The monthly payment for this loan is:$ %.2f" % monthly_payment)
+
+main()
